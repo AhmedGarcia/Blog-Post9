0f8c3e90a6a53697361d0470ffdf87bb8dcce0a0
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-06-25 21:08:32 -0400

    added code to logic.js to get light and dark mode working properly

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,12 @@
 document.getElementById('modeToggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
+
+    const modeIcon = document.querySelector('#modeToggle i');
+    if (document.body.classList.contains('dark-mode')) {
+        modeIcon.classList.remove('fa-sun');
+        modeIcon.classList.add('fa-moon');
+    } else {
+        modeIcon.classList.remove('fa-moon');
+        modeIcon.classList.add('fa-sun');
+    }
 });
